f8d8dcc506c70e19475904d2149f47f778adc2db
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-03-13 10:14:51 -0500

    first commit

---
+++ src/main/java/com/codeup/srpingblog/SrpingblogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.srpingblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpingblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/srpingblog/SrpingblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.srpingblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
