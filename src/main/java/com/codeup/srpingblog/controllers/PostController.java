b8a33b4bb58744435b446d11093853fe9e3514a1
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-03-16 11:19:49 -0500

    exercise done

--- src/main/java/com/codeup/srpingblog/controllers/PostController.java
+++ src/main/java/com/codeup/srpingblog/controllers/PostController.java
@@ -13,21 +13,23 @@ import java.util.List;
 class PostController {
 
     @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    public String posts(){
+    public String getPosts(Model model){
         List<Post> postList = new ArrayList<>();
 
-        postList.add(new Post("Title 1", "Body 1"));
-        postList.add(new Post("Title 2", "Body 2"));
-        return "/posts/index";
+        postList.add(new Post(2, "Title 2", "Body 2"));
+        postList.add(new Post(3, "Title 3", "Body 3"));
+
+        model.addAttribute("posts", postList); //pass list
+        return "posts/index";
     }
 
 
     @RequestMapping (path = "/posts/{id}", method = RequestMethod.GET)
-    public String postsId(@PathVariable int id, Model model){
-        Post post = new Post( );
-
-        model.addAttribute(post);
-        return "/posts/show";
+    public String getPost(@PathVariable int id, Model model){
+        Post post1 = new Post(id, "first post", "remote learning");
+        model.addAttribute("title", post1.getTitle());
+        model.addAttribute("body", post1.getBody());
+        return "posts/show";
     }
 
     @RequestMapping (path = "/posts/create", method = RequestMethod.GET)
--- src/main/java/com/codeup/srpingblog/models/Post.java
+++ src/main/java/com/codeup/srpingblog/models/Post.java
@@ -1,6 +1,7 @@
 package com.codeup.srpingblog.models;
 
 public class Post {
+    private long id;
     private String title;
     private String body;
 
@@ -10,12 +11,17 @@ public class Post {
     }
 
     //CONSTRUCTOR
-    public Post(String title, String body) {
+    public Post(long id, String title, String body) {
+        this.id = id;
         this.title = title;
         this.body = body;
     }
 
     //GETTERS
+
+    public long getId() {
+        return id;
+    }
     public String getTitle() {
         return this.title;
     }
@@ -24,6 +30,10 @@ public class Post {
     }
 
     //SETTERS
+
+    public void setId(long id) {
+        this.id = id;
+    }
     public void setTitle(String title) {
         this.title = title;
     }
